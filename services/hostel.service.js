cc79a7e6cd0a8a6c27accb885905cea366458774
Israel Quiroz <israquirozz@MacBook-Pro-de-Israel.local>
2026-06-05 01:01:31 +0200

    feat: fix typo: hostals -> hostels

--- services/hostel.service.js
+++ services/hostel.service.js
@@ -2,28 +2,28 @@ import api from "./api";
 
 export const hostelService = {
   async getCities() {
-    const { data } = await api.get("/hostals/cities");
+    const { data } = await api.get("/hostels/cities");
     return data.data;
   },
 
   async getHostels(search = "") {
-    const url = search ? `/hostals?city=${search}` : "/hostals";
+    const url = search ? `/hostels?city=${search}` : "/hostels";
     const { data } = await api.get(url);
     return data.data;
   },
 
   async getHostelsByCity(cityName) {
-    const { data } = await api.get(`/hostals?city=${cityName}`);
+    const { data } = await api.get(`/hostels?city=${cityName}`);
     return data.data;
   },
 
   async getTopHostels() {
-    const { data } = await api.get("/hostals/top");
+    const { data } = await api.get("/hostels/top");
     return data.data;
   },
 
   async getHostelById(id) {
-    const { data } = await api.get(`/hostals/${id}`);
+    const { data } = await api.get(`/hostels/${id}`);
     return data.data;
   },
 };
